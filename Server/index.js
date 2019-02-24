985310a6a644bed3eafb7381349e4e2b53f4bfa0
Giuseppe Fagnani <gfagnani.job@gmail.com>
2019-02-24 19:19:42 +0100

    aggiunto db locale (con campi di prova) e implementate query

--- Server/index.js
+++ Server/index.js
@@ -1,40 +1,8 @@
 const express = require('express');
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
 
 const PORTA = 3000;
-
-/* \\\\\\\\ FAKE DB ///////// */
-const utenti = {
-  'utenti': [
-    {
-      'username': 'admin',
-      'password': 'admin'
-    },
-    {
-      'username': 'studente',
-      'password': 'studente'
-    },
-    {
-      'username': 'proprietario',
-      'password': 'proprietario'
-    }
-  ]
-}
-
-const annunci = {
-  'annunci': [
-    {
-      'id': '1',
-      'descrizione': 'una bella casa',
-      'prezzo': '200€'
-    },
-    {
-      'id': '2',
-      'descrizione': 'una casa brutta',
-      'prezzo': '150€'
-    }
-  ]
-}
 
 /* \\\\\\\\ AVVIA IL SERVER ///////// */
 app.listen(PORTA, () => {
@@ -45,7 +13,33 @@ app.listen(PORTA, () => {
 
 //ritorna la lista di tutti gli utenti su db
 app.get('/api/utenti', (req, res) => {
-  res.send(utenti);
+  let utenti = [];
+
+  let db = new sqlite3.Database('../db/affitti.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connesso al database affitti.db.');
+  });
+
+  let sql = 'SELECT * FROM utenti';
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      utenti.push(row);
+    });
+    res.json({ 'utenti': utenti });
+  });
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
 });
 
 //ritorna uno specifico utente (ricercato per username)
@@ -53,13 +47,39 @@ app.get('/api/utenti/:username', (req, res, next) => {
   var username = req.params.username;
   trovaUtentePerUsername(username, (err, user) => {
     if (err) return next(err);
-    res.send({'utente': user});
+    res.send({ 'utente': user });
   });
 });
 
 //ritorna la lista di tutti gli annunci su db
 app.get('/api/annunci', (req, res) => {
-  res.send(annunci);
+  let annunci = [];
+
+  let db = new sqlite3.Database('../db/affitti.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connesso al database affitti.db.');
+  });
+
+  let sql = 'SELECT * FROM annunci';
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      annunci.push(row);
+    });
+    res.json({ 'annunci': annunci });
+  });
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
 });
 
 //ritorna uno specifico annuncio (ricercato per id)
@@ -79,38 +99,105 @@ app.get('/api/annunci/:id', (req, res, next) => {
 app.post('/api/annunci', (req, res) => {
   //cambiare le seguenti righe prendendo i dati dal body (invece che dal fake body)
   //fake body
-  var newID = annunci.annunci.length + 1;
-  var newDescrizione = 'una nuova casa ' + newID;
-  var newPrezzo = newID * 75;
-  annunci.annunci.push({ newID, newDescrizione, newPrezzo });
-  res.send(annunci);
+  var newTitolo = 'una nuova casa ' + Math.random() * 100;
+  var newDescrizione = 'descrizione ' + Math.random() * 100;
+  var newFoto = 'linkFoto';
+  var newPrezzo = Math.random() * 200;
+  var newIsDisponibile = "true";
+
+  let db = new sqlite3.Database('../db/affitti.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connesso al database affitti.db.');
+  });
+
+  let sql = 'INSERT INTO annunci (titolo, descrizione, foto, prezzo, isDisponibile) VALUES ("' +
+    newTitolo + '", "' +
+    newDescrizione + '", "' +
+    newFoto + '", ' +
+    newPrezzo + ', "' +
+    newIsDisponibile + '")';
+
+  db.run(sql, [], function (err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    console.log('A row has been inserted');
+  });
+  // annunci.annunci.push({ newID, newDescrizione, newPrezzo });
+  // res.send(annunci);
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+
+  res.send({'status': 'ok'});
+
 });
 
 
 /* \\\\\\\\ FUNZIONI DI SUPPORTO ////////// */
 
 var trovaUtentePerUsername = function (username, callback) {
-  // Sostituire le seguendi righe con una query su db (invece che su fake db)
-  var listaUtenti = utenti.utenti;
-  var trovato = false;
-  listaUtenti.forEach(element => {
-    if (element.username == username) {
-      trovato = true;
-      return callback (null, element);
+  let utente = {};
+
+  let db = new sqlite3.Database('../db/affitti.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
     }
+    console.log('Connesso al database affitti.db.');
   });
-  if (!trovato) return callback (new Error('Nessun utente trovato con username: ' + username));
+
+  let sql = 'SELECT * FROM utenti WHERE utenti.username = "' + username + '"';
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      utente = row;
+    });
+    return callback(null, utente)
+  });
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
 };
 
 var trovaAnnuncioPerId = function (id, callback) {
-  // Sostituire le seguendi righe con una query su db (invece che su fake db)
-  var listaAnnunci = annunci.annunci;
-  var trovato = false;
-  listaAnnunci.forEach(element => {
-    if (element.id == id) {
-      trovato = true;
-      return callback (null, element);
+  let annuncio = {};
+
+  let db = new sqlite3.Database('../db/affitti.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
     }
+    console.log('Connesso al database affitti.db.');
   });
-  if (!trovato) return callback (new Error('Nessun annuncio trovato con id: ' + id));
+
+  let sql = 'SELECT * FROM annunci WHERE annunci.id = ' + id;
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      annuncio = row;
+    });
+    return callback(null, annuncio)
+  });
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
 };
